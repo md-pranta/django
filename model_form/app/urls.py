ba0b2503c22402c440f36bb11f9212f4eff07874
md-pranta <juhurenurulhaque@gmail.com>
2023-12-04 23:07:55 +0600

    delete data from fronend

--- model_form/app/urls.py
+++ model_form/app/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from .views import *
 
 urlpatterns = [
-    path('', home),
+    path('', home, name="home"),
+    path('delete/<int:roll>', delete, name="delete"),
 ]
--- model_form/app/views.py
+++ model_form/app/views.py
@@ -1,7 +1,12 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .models import *
 # Create your views here.
 def home(req):
     student = Student.objects.all()
     # print(Student)
     return render(req, 'home.html', {'data':student})
+    
+def delete(req, roll):
+    sde = Student.objects.get(pk = roll).delete()
+    # print(sde)
+    return redirect("home")
