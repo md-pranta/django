28b6d01674f2b5460e1596eeb33cc95c6dfc9be1
md-pranta <juhurenurulhaque@gmail.com>
2023-12-30 09:17:03 +0600

    done

---
+++ mamaBank_prac2/mamar_bank/transactions/constants.py
@@ -0,0 +1,15 @@
+DEPOSIT = 1
+WITHDRAWAL = 2
+LOAN = 3
+LOAN_PAID = 4
+SEND_MONEY = 5
+RECIVE_MONEY = 6
+TRANSACTION_TYPE = (
+    (DEPOSIT, 'Deposite'),
+    (WITHDRAWAL, 'Withdrawal'),
+    (LOAN, 'Loan'),
+    (LOAN_PAID, 'Loan Paid'),
+    (SEND_MONEY,'SEND_MONEY'),
+    (RECIVE_MONEY,'RECIVE_MONEY'),
+    
+)
